6002a7afef51a6db74fc33cec7d370b9ec446852
Jose Mendonca <jmendonca@gmail.com>
2026-09-15 17:03:16 +0100

    Favicon and logo app icon change

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,7 +5,6 @@
     {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
 </title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
